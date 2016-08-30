18d203f646a6e282be2c8694cae469d1fba8abfc
kaznishi <kaz.nishimu@gmail.com>
2016-08-31 08:48:36 +0900

    shadeとslickの導入

--- app/controllers/HomeController.scala
+++ app/controllers/HomeController.scala
@@ -18,6 +18,18 @@ class HomeController @Inject() extends Controller {
    * a path of `/`.
    */
   def index = Action {
+
+//    import shade.memcached._
+//    import scala.concurrent.ExecutionContext.Implicits.global
+//    import scala.concurrent.Future
+//    import scala.concurrent.duration._
+//
+//    val memcached = Memcached(Configuration("127.0.0.1:11211"))
+//    val op: Future[Unit] = memcached.set("username", "Alex", 1.minute)
+//    val result: Future[Option[String]] = memcached.get[String]("username")
+//
+//    result.map(println(_))
+
     Ok(views.html.index("Your new application is ready."))
   }
 
--- build.sbt
+++ build.sbt
@@ -10,6 +10,10 @@ libraryDependencies ++= Seq(
   jdbc,
   cache,
   ws,
+  "mysql" % "mysql-connector-java" % "5.1.36",
+  "com.typesafe.play" %% "play-slick" % "2.0.0",
+//  "com.typesafe.play" %% "play-slick-evolutions" % "2.0.0",
+  "com.bionicspirit" %% "shade" % "1.7.4",
   "org.scalatestplus.play" %% "scalatestplus-play" % "1.5.1" % Test,
   "com.github.gilbertw1" %% "slack-scala-client" % "0.1.7"
 )
