fc1580327316790d103c6215da741104745d7a89
kaznishi <kaz.nishimu@gmail.com>
2016-09-06 23:48:13 +0900

    dummy api 仕様作成

--- app/controllers/DummyYatoController.scala
+++ app/controllers/DummyYatoController.scala
@@ -16,27 +16,61 @@ class DummyYatoController @Inject()(json4s: Json4s)
   import json4s._
   implicit val formats = DefaultFormats
 
-  // /yato-bot/user-info/by-email/[email]
+  // /yato-bot-api/v1/user/info/?email=[email]
   def userInfoByEmail(email: String) = Action.async { implicit request =>
+    val err = YatoErrorResponse(false, "データ取得に失敗しました。", Seq(YatoError("存在しないユーザです")))
+
     val u = YatoUser("hogename", "hoge@example.com", true)
-    Future.successful(1).map(r => Ok(Extraction.decompose("data" -> u)))
+    val res = YatoUserInfoByEmailSuccessResponse(true, u)
+
+    Future.successful(1).map(r => Ok(Extraction.decompose(err)))
+//    Future.successful(1).map(r => Ok(Extraction.decompose(res)))
   }
 
-  // /yato-bot/point/by-email/[email]
+  // /yato-bot-api/v1/user/point/?email=[email]
   def availablePointByEmail(email: String) = Action.async { implicit request =>
-    Future.successful().map(_ => Ok(Extraction.decompose("data" -> YatoAvailablePoint(100))))
+    val err = YatoErrorResponse(false, "データ取得に失敗しました。", Seq(YatoError("存在しないユーザです")))
+
+    val p = YatoAvailablePoint(100)
+    val res = YatoAvailablePointByEmailSuccessResponse(true, p)
+
+//    Future.successful().map(_ => Ok(Extraction.decompose(err)))
+    Future.successful().map(_ => Ok(Extraction.decompose(res)))
   }
 
-  // /yato-bot/give [post]fromUserId,toUserId,message,valueId,point
-  def give = Action.async { implicit request =>
-    println(request)
-    println(request.body.asJson.getOrElse(""))
-//    println(parse(""" { "numbers" : [1, 2, 3, 4] } """))
+  // /yato-bot-api/v1/admiration/post [post]fromUserEmail,toUserEmail,message,valueId,point
+  def postAdmiration = Action.async(json) { implicit request =>
+    val err = YatoErrorResponse(
+      false,
+      "登録に失敗しました。",
+      Seq(
+        YatoError("送り主ユーザが存在しません"),
+        YatoError("送り先ユーザが存在しません"),
+        YatoError("この人にはポイントを送れません"),
+        YatoError("メッセージの文字数がオーバーしています"),
+        YatoError("ポイントが足りません")
+      )
+    )
 
-    Future.successful().map(_ => Ok(Extraction.decompose("data" -> YatoAvailablePoint(100))))
+    val admirationRequest = request.body.extract[YatoAdmirationRequest]
+    println(admirationRequest)
+
+//    Future.successful().map(_ => Ok(Extraction.decompose(YatoAdmirationSuccessResponse(true))))
+    Future.successful().map(_ => Ok(Extraction.decompose(err)))
   }
 
 }
 
 case class YatoUser(name: String, email: String, isPointAcceptable: Boolean)
 case class YatoAvailablePoint(point: Int)
+
+case class YatoAdmirationRequest(fromUserEmail: String, toUserEmail: String, message: String, valueId: Int, point: Int)
+
+case class YatoError(message: String)
+case class YatoErrorResponse(status: Boolean, message: String, errors: Seq[YatoError])
+case class YatoUserInfoByEmailSuccessResponse(status: Boolean, data: YatoUser)
+case class YatoAvailablePointByEmailSuccessResponse(status: Boolean, data: YatoAvailablePoint)
+case class YatoAdmirationSuccessResponse(status: Boolean)
+
+
+
